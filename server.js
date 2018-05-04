47a4d41fc0d3b5b3c1cff54f51c8aa9c1adc86d9
Pete LaMalfa <lamalfa.peter@gmail.com>
2018-05-04 14:27:28 -0400

    replaced server file

--- server.js
+++ server.js
@@ -9,57 +9,69 @@ const PORT = process.env.PORT || 3000;
 
 var Discogs = require("disconnect").Client;
 
-let _requestData, _accessData;
+let _requestData;
+
+const _accessData = {
+  method: "oauth",
+  level: 2,
+  consumerKey: "ScTvSOsxTpJNiempUWhF",
+  consumerSecret: "kvDwtbQJysQWRjUOFQGvYUsXrDdELbYx",
+  token: "HnqSpHUCoQuDkYkMrUhjBMlVshUliChGxUMgqBzj",
+  tokenSecret: "HIMdGuOVlqXsSuhzoNDNUpsyuirkAuaTjJLBqJBz"
+};
 
 // var db = new Discogs().database();
 // db.getRelease(176126, function(err, data) {
 //   console.log(data);
 // });
 
-app.get("/authorize", function(req, res) {
-  var oAuth = new Discogs().oauth();
-  oAuth.getRequestToken(
-    "ScTvSOsxTpJNiempUWhF",
-    "kvDwtbQJysQWRjUOFQGvYUsXrDdELbYx",
-    "https://sheltered-beyond-73183.herokuapp.com/callback",
-    function(err, requestData) {
-      _requestData = requestData;
-      // Persist "requestData" here so that the callback handler can
-      // access it later after returning from the authorize url
-      res.redirect(requestData.authorizeUrl);
-    }
-  );
-});
+//Below, this is the stuff I have to do to get _accessData back from discogs
+//now that we have it, I will hardcode it in
 
-app.get("/callback", function(req, res) {
-  var oAuth = new Discogs(_requestData).oauth();
-  oAuth.getAccessToken(
-    req.query.oauth_verifier, // Verification code sent back by Discogs
-    function(err, accessData) {
-      _accessData = accessData;
-      // Persist "accessData" here for following OAuth calls
-      res.redirect("https://sheltered-beyond-73183.herokuapp.com/identity");
-    }
-  );
-});
+// app.get("/authorize", function(req, res) {
+//   var oAuth = new Discogs().oauth();
+//   oAuth.getRequestToken(
+//     "ScTvSOsxTpJNiempUWhF",
+//     "kvDwtbQJysQWRjUOFQGvYUsXrDdELbYx",
+//     "http://localhost:3000/callback",
+//     function(err, requestData) {
+//       _requestData = requestData;
+//       // Persist "requestData" here so that the callback handler can
+//       // access it later after returning from the authorize url
+//       res.redirect(requestData.authorizeUrl);
+//     }
+//   );
+// });
 
-app.get("/identity", function(req, res) {
-  var dis = new Discogs(_accessData);
-  dis.getIdentity(function(err, data) {
-    res.send(data);
-    console.log(_accessData);
-    getDiscogSearch();
-  });
-});
+// app.get("/callback", function(req, res) {
+//   var oAuth = new Discogs(_requestData).oauth();
+//   oAuth.getAccessToken(
+//     req.query.oauth_verifier, // Verification code sent back by Discogs
+//     function(err, accessData) {
+//       _accessData = accessData;
+//       // Persist "accessData" here for following OAuth calls
+//       res.redirect("/identity");
+//     }
+//   );
+// });
 
-const getDiscogSearch = () => {
+// app.get("/identity", function(req, res) {
+//   var dis = new Discogs(_accessData);
+//   dis.getIdentity(function(err, data) {
+//     res.send(data);
+//     console.log(_accessData);
+//     getDiscogSearch();
+//   });
+// });
+
+app.post("/search", function(req, res) {
   let db = new Discogs(_accessData).database();
-  db.search("the strokes room on fire", (err, data) => {
+  db.search(req.query, (err, data) => {
     var searchResults = data.results;
     var firstFiveResults = searchResults.slice(0, 5);
     console.log(firstFiveResults);
   });
-};
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
